551e5e1547c573192b6cfaadcdbbaffc18fd498b
Daria Kroshka <146644868+luvremak@users.noreply.github.com>
2024-10-06 13:46:18 +0300

    Create lab1.2(1).c

---
+++ 1/lab1.2/lab1.2(1).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    int n;
+    printf("Whats n?: \n");
+    scanf("%d", &n);
+    if (n <= 0) {
+        printf("Please enter a valid value for n\n");
+    } else {
+    
+    }
+
+    double P;
+    
+}
